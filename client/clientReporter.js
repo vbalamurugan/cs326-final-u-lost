3770ee35c3914d5f0bbb684f4f36292027b2885e
sahiljoshi515 <sajoshi@umass.edu>
2022-04-08 16:14:24 -0400

    Push

--- client/clientReporter.js
+++ client/clientReporter.js
@@ -25,3 +25,78 @@ document.getElementById("deleteButton").addEventListener("click", async (e) => {
     const id = document.getElementById("id2").value;
     const newItem = await crud.deleteItem(id);
 });
+
+function CreateTableFromJSON() {
+    const jsonob1 = { category: "electronics", location: "library", contact: "Sid", time: "7:20pm", image: "image1", id: "1" };
+    const jsonob2 = { category: "mobile", location: "lgrc", contact: "Me", time: "5:40pm", image: "image2", id: "2" };
+    const jsonob3 = { category: "wallet", location: "office", contact: "Ghost", time: "9:40pm", image: "image3", id: "3" };
+
+    let myBooks = [];
+    myBooks.push(jsonob1, jsonob2, jsonob3);
+
+    // let myBooks = [{
+    //         "Book ID": "1",
+    //         "Book Name": "Computer Architecture",
+    //         "Category": "Computers",
+    //         "Price": "125.60"
+    //     },
+    //     {
+    //         "Book ID": "2",
+    //         "Book Name": "Asp.Net 4 Blue Book",
+    //         "Category": "Programming",
+    //         "Price": "56.00"
+    //     },
+    //     {
+    //         "Book ID": "3",
+    //         "Book Name": "Popular Science",
+    //         "Category": "Science",
+    //         "Price": "210.40"
+    //     }
+    // ]
+
+    // EXTRACT VALUE FOR HTML HEADER. 
+    // ('Book ID', 'Book Name', 'Category' and 'Price')
+    var col = [];
+    for (var i = 0; i < myBooks.length; i++) {
+        for (var key in myBooks[i]) {
+            if (col.indexOf(key) === -1) {
+                col.push(key);
+            }
+        }
+    }
+
+    // CREATE DYNAMIC TABLE.
+    // var table = document.createElement("table");
+    let table = document.getElementById("tabledata")
+    table.classList.add('table');
+    table.classList.add('table-hover');
+    // CREATE HTML TABLE HEADER ROW USING THE EXTRACTED HEADERS ABOVE.
+
+    var tr = table.insertRow(-1); // TABLE ROW.
+    tr.classList.add('cell');
+    for (var i = 0; i < col.length; i++) {
+        var th = document.createElement("th"); // TABLE HEADER.
+        th.innerHTML = col[i];
+        tr.appendChild(th);
+    }
+
+    // ADD JSON DATA TO THE TABLE AS ROWS.
+    for (var i = 0; i < myBooks.length; i++) {
+
+        tr = table.insertRow(-1);
+
+        for (var j = 0; j < col.length; j++) {
+            var tabCell = tr.insertCell(-1);
+            tabCell.setAttribute("data-bs-toggle", "modal");
+            tabCell.setAttribute("data-bs-target", "#exampleModal");
+            tabCell.innerHTML = myBooks[i][col[j]];
+        }
+    }
+
+    // FINALLY ADD THE NEWLY CREATED TABLE WITH JSON DATA TO A CONTAINER.
+    var divContainer = document.getElementById("span");
+    divContainer.innerHTML = "";
+    divContainer.appendChild(table);
+}
+
+CreateTableFromJSON();
