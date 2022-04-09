77c589a06f001fc72bcf6b4780c228947742ba92
vbalamurugan <vbalamurugan@umass.edu>
2022-04-09 14:06:27 -0400

    fixed buttonRouting logic

---
+++ client/buttonrouting2.js
@@ -0,0 +1,8 @@
+import * as crud from "./crud.js";
+
+document.getElementById("reporter-btn").onclick = function () {
+    location.href = "http://localhost:3000/client/categories.html";
+};
+document.getElementById("finder-btn").onclick = function () {
+    location.href = "http://localhost:3000/client/categories.html";
+};
