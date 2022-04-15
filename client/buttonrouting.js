c57f938a6571d182fbb79a6964a101a7a0639747
DACSS <sidharthsalu@umass.edu>
2022-04-15 15:26:31 -0400

    done

--- client/buttonrouting.js
+++ client/buttonrouting.js
@@ -1,15 +1,14 @@
 import * as crud from "./crud.js";
 
-document.getElementById("submit").addEventListener("click", async (e) => {
+document.getElementById("submit").addEventListener("click", async(e) => {
     const email = document.getElementById("email").value;
     const passcode = document.getElementById("passcode").value;
     const loginthing = await crud.readLogin(email, passcode);
 
     if (loginthing.error) {
         console.log("error");
-    }
-    else {
+    } else {
         console.log("no error");
-        location.href = "https://radiant-wave-67942.herokuapp.com/client/reporterFinder.html"
+        location.href = "https://immense-fjord-85744.herokuapp.com/client/reporterFinder.html"
     }
 });
--- client/buttonrouting3.js
+++ client/buttonrouting3.js
@@ -1,18 +1,18 @@
-document.getElementById("electronics-btn").onclick = function () {
-    location.href = "https://radiant-wave-67942.herokuapp.com/client/itemListFinder.html";
+document.getElementById("electronics-btn").onclick = function() {
+    location.href = "https://immense-fjord-85744.herokuapp.com/client/itemListFinder.html";
 };
-document.getElementById("idcards-btn").onclick = function () {
-    location.href = "https://radiant-wave-67942.herokuapp.com/client/itemListFinder.html";
+document.getElementById("idcards-btn").onclick = function() {
+    location.href = "https://immense-fjord-85744.herokuapp.com/client/itemListFinder.html";
 };
-document.getElementById("keys-btn").onclick = function () {
-    location.href = "https://radiant-wave-67942.herokuapp.com/client/itemListFinder.html";
+document.getElementById("keys-btn").onclick = function() {
+    location.href = "https://immense-fjord-85744.herokuapp.com/client/itemListFinder.html";
 };
-document.getElementById("books-btn").onclick = function () {
-    location.href = "https://radiant-wave-67942.herokuapp.com/client/itemListFinder.html";
+document.getElementById("books-btn").onclick = function() {
+    location.href = "https://immense-fjord-85744.herokuapp.com/client/itemListFinder.html";
 };
-document.getElementById("clothing-btn").onclick = function () {
-    location.href = "https://radiant-wave-67942.herokuapp.com/client/itemListFinder.html";
+document.getElementById("clothing-btn").onclick = function() {
+    location.href = "https://immense-fjord-85744.herokuapp.com/client/itemListFinder.html";
 };
-document.getElementById("other-btn").onclick = function () {
-    location.href = "https://radiant-wave-67942.herokuapp.com/client/itemListFinder.html";
+document.getElementById("other-btn").onclick = function() {
+    location.href = "https://immense-fjord-85744.herokuapp.com/client/itemListFinder.html";
 };
