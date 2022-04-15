dd82c47236cb41c7af6008a02007d8900fc8a760
DACSS <sidharthsalu@umass.edu>
2022-04-15 16:04:48 -0400

    done

--- client/buttonrouting.js
+++ client/buttonrouting.js
@@ -2,13 +2,18 @@ import * as crud from "./crud.js";
 
 document.getElementById("submit").addEventListener("click", async(e) => {
     const email = document.getElementById("email").value;
-    const passcode = document.getElementById("passcode").value;
-    const loginthing = await crud.readLogin(email, passcode);
+    if (!email.includes("@")) {
+        alert("Email invalid found")
 
-    if (loginthing.error) {
-        console.log("error");
     } else {
-        console.log("no error");
-        location.href = "https://immense-fjord-85744.herokuapp.com/client/reporterFinder.html"
+        const passcode = document.getElementById("passcode").value;
+        const loginthing = await crud.readLogin(email, passcode);
+        if (loginthing.error) {
+            console.log("error");
+        } else {
+            console.log("no error");
+            location.href = "https://immense-fjord-85744.herokuapp.com/client/reporterFinder.html"
+        }
     }
+
 });
