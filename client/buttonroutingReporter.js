d14e6b18728801d30f39b66d3b47135fed9032aa
DACSS <sidharthsalu@umass.edu>
2022-04-16 15:09:38 -0400

    deployed

---
+++ client/buttonroutingReporter.js
@@ -0,0 +1,18 @@
+document.getElementById("electronics-btn").addEventListener("click", function() {
+    location.href = "https://immense-fjord-85744.herokuapp.com/client/itemListReporter.html";
+});
+document.getElementById("idcards-btn").addEventListener("click", function() {
+    location.href = "https://immense-fjord-85744.herokuapp.com/client/itemListReporter.html";
+});
+document.getElementById("keys-btn").addEventListener("click", function() {
+    location.href = "https://immense-fjord-85744.herokuapp.com/client/itemListReporter.html";
+});
+document.getElementById("books-btn").addEventListener("click", function() {
+    location.href = "https://immense-fjord-85744.herokuapp.com/client/itemListReporter.html";
+});
+document.getElementById("clothing-btn").addEventListener("click", function() {
+    location.href = "https://immense-fjord-85744.herokuapp.com/client/itemListReporter.html";
+});
+document.getElementById("other-btn").addEventListener("click", function() {
+    location.href = "https://immense-fjord-85744.herokuapp.com/client/itemListReporter.html";
+});
---
+++ client/buttonroutingUserType.js
@@ -0,0 +1,6 @@
+document.getElementById("reporter-btn").addEventListener("click", function() {
+    location.href = "https://immense-fjord-85744.herokuapp.com/client/categoriesReporter.html";
+});
+document.getElementById("finder-btn").addEventListener("click", function() {
+    location.href = "https://immense-fjord-85744.herokuapp.com/client/categoriesFinder.html";
+});
