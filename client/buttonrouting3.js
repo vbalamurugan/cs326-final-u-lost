f785ca11de45936fad91e3c189db07d1c829c434
vbalamurugan <vbalamurugan@umass.edu>
2022-04-21 14:13:00 -0400

    local commit

---
+++ client/buttonrouting3.js
@@ -0,0 +1,50 @@
+import{getUserType} from buttonrouting2.js
+
+document.getElementById("electronics-btn").onclick = function() {
+    userType = getUserType()
+    if(userType === "finder"){
+        location.href = "https://immense-fjord-85744.herokuapp.com/client/itemListFinder.html";
+    }else{
+        location.href = "https://immense-fjord-85744.herokuapp.com/client/itemListReporter.html";
+    }
+};
+document.getElementById("idcards-btn").onclick = function() {
+    userType = getUserType()
+    if(userType === "finder"){
+        location.href = "https://immense-fjord-85744.herokuapp.com/client/itemListFinder.html";
+    }else{
+        location.href = "https://immense-fjord-85744.herokuapp.com/client/itemListReporter.html";
+    }
+};
+document.getElementById("keys-btn").onclick = function() {
+    userType = getUserType()
+    if(userType === "finder"){
+        location.href = "https://immense-fjord-85744.herokuapp.com/client/itemListFinder.html";
+    }else{
+        location.href = "https://immense-fjord-85744.herokuapp.com/client/itemListReporter.html";
+    }
+};
+document.getElementById("books-btn").onclick = function() {
+    userType = getUserType()
+    if(userType === "finder"){
+        location.href = "https://immense-fjord-85744.herokuapp.com/client/itemListFinder.html";
+    }else{
+        location.href = "https://immense-fjord-85744.herokuapp.com/client/itemListReporter.html";
+    }
+};
+document.getElementById("clothing-btn").onclick = function() {
+    userType = getUserType()
+    if(userType === "finder"){
+        location.href = "https://immense-fjord-85744.herokuapp.com/client/itemListFinder.html";
+    }else{
+        location.href = "https://immense-fjord-85744.herokuapp.com/client/itemListReporter.html";
+    }
+};
+document.getElementById("other-btn").onclick = function() {
+    userType = getUserType()
+    if(userType === "finder"){
+        location.href = "https://immense-fjord-85744.herokuapp.com/client/itemListFinder.html";
+    }else{
+        location.href = "https://immense-fjord-85744.herokuapp.com/client/itemListReporter.html";
+    }
+};
